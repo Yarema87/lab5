3d23a7a5465e16c8c7eed8954377211d3f59fffe
Yarema87 <gekon4uk@gmail.com>
2022-12-19 00:02:27 +0200

    adding cleaner code

--- main.cpp
+++ main.cpp
@@ -1,64 +1,67 @@
 #include <iostream>
+
 class Musician {
 private:
     std::string name;
     int price;
 public:
-    Musician(std::string name1,int price1){
-        name=name1;
-        price=price1;
+    Musician(std::string name1, int price1){
+        name = name1;
+        price = price1;
     }
     std::string get_name() {return name;};
-    int get_price(){return price;};
+    int get_price() {return price;};
 };
-class Music_Festival {
+
+class MusicFestival {
 private:
     int budjet;
     std::string musicians[10];
     int prices[10];
 public:
-    std::string get_musicians(){return musicians[10];};
+    std::string get_musicians(){return musicians[];};
     int get_budjet(){return budjet;};
-    int get_prices(){return prices[10];};
-    Music_Festival(int budjet1){
+    int get_prices(){return prices[];};
+    MusicFestival(int budjet1){
         budjet=budjet1;
     }
-    Music_Festival() {
+    MusicFestival() {
         budjet = 5;
     }
     friend void add_musician();
 };
+
 void add_musician() {
     int sum=0;
-    int i=0;
-    Music_Festival music_festival1(5);
+    MusicFestival musicFestival1(5);
     for (int j=0; j<=5; j++) {
         int price[10];
         std::cin >> price[j];
         std::string name[5];
         std::cin >> name[j];
         Musician musician1(name[j], price[j]);
-        if (musician1.get_price() <= music_festival1.get_budjet()) {
-            music_festival1.musicians[j] = musician1.get_name();
-            music_festival1.prices[j]= musician1.get_price();
-        } else if (musician1.get_price() > music_festival1.get_budjet())
+        if (musician1.get_price() <= musicFestival1.get_budjet()) {
+            musicFestival1.musicians[j] = musician1.get_name();
+            musicFestival1.prices[j] = musician1.get_price();
+        } else if (musician1.get_price() > musicFestival1.get_budjet())
         {
             std::cout<<musician1.get_name()<<" is too expensive for our concert"<<std::endl;
             continue;
         }
     }
-    for (;i<=5;i++){
-        if ((music_festival1.prices[i]+sum<=music_festival1.get_budjet())) {
-            std::cout << "we can afford " << music_festival1.musicians[i] << " for our concert," << std::endl;
+    for (int i = 0;i<=5;i++){
+        if ((musicFestival1.prices[i]+sum<=musicFestival1.get_budjet())) {
+            std::cout << "we can afford " << musicFestival1.musicians[i] << " for our concert," << std::endl;
         }
-        else if ((music_festival1.musicians[i]!="")&&(music_festival1.musicians[i+1]=="")&&(music_festival1.prices[i]+sum<=music_festival1.get_budjet())&&(music_festival1.prices[i+1]+sum<=music_festival1.get_budjet())){
-            std::cout << "we can afford " << music_festival1.musicians[i] << " for our concert.";
+        else if ((musicFestival1.musicians[i]!="")&&(musicFestival1.musicians[i+1]=="")&&(musicFestival1.prices[i]+sum<=musicFestival1.get_budjet())&&(musicFestival1.prices[i+1]+sum<=musicFestival1.get_budjet())){
+            std::cout << "we can afford " << musicFestival1.musicians[i] << " for our concert.";
         }
         else
             continue;
-        sum+=music_festival1.prices[i];
+        sum+=musicFestival1.prices[i];
     }
 }
+
 int main(){
     add_musician();
     return 0;
